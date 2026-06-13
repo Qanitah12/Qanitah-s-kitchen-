37a5d62a5fe284dc4c2a76e2c8e616edc9ca477e
Qanitah12 <shile043@gmail.com>
2026-06-13 14:27:35 +0100

    edit view

---
+++ script.js
@@ -0,0 +1,2 @@
+alert ("Welcome to Qanitah's Kitchen! We are delighted to have you here. Explore our delicious dishes, check out our schedule for upcoming food, and feel free to contact us for any inquiries. Enjoy your culinary journey with us!");
+
